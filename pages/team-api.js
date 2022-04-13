f4a2ac57d768ae2c8ac5d3eef6a4889061fa0826
Mulumia Baraka Mshindi <barakamulumia@gmail.com>
2022-04-13 13:54:48 +0300

    Add api  url link

--- pages/team-api.js
+++ pages/team-api.js
@@ -1,19 +1,30 @@
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, Link, Text, VStack } from "@chakra-ui/react";
 
 import HeaderSection from "../components/HeaderSection";
 
 export default function Home() {
 	return (
-		<Box>
-			<main>
+		<Box w={"full"}>
+			<main w={"full"}>
 				<HeaderSection />
 				<VStack
 					bg={"whiteAlpha.900"}
 					h={"calc(100vh - 100px)"}
 					justify={"center"}
+					w={"full"}
 				>
-					<Text w={"80%"} p={4} fontWeight={500} fontSize={"2xl"}>
-						API DOCUMENTATION COMING SOON Visit [API_URL]
+					<Text w={"50%"} p={4} fontWeight={500} fontSize={"2xl"}>
+						API DOCUMENTATION COMING SOON Visit <br />
+						<Link
+							color={"slateblue"}
+							href={
+								"https://ist-developers-platform.vercel.app/api/team"
+							}
+							target={"_blank"}
+							rel={"noreferrer"}
+						>
+							https://ist-developers-platform.vercel.app/api/team
+						</Link>
 					</Text>
 				</VStack>
 			</main>
